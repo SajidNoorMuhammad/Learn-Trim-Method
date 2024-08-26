a0149724a0d6218b4b5f0cef05ae47fe947eecc7
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-08-26 14:45:38 +0500

    practice

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const word = "Hello,    World  "
+const trimmed= word.trim();
+document.write(trimmed)
 
-document.write(word.trim())
 
-const input = document.getElementById("input");
 
-if(input.trim() != ''){
-    console.log("Already Trim")
-}
 
+
+const fake = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum hic dolorem ipsam at, in harum     culpa sapiente.     Omnis quis nam earum   totam quod est. Ut od it cumque odio modi repellat.";
+const faketrimmed= fake.trim();
+document.write(faketrimmed)
